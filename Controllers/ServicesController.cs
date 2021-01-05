f00613b2c8a5304463b1855af8f46b4139ecd7ff
redes <>
2021-01-05 17:54:47 -0600

    Creacion de ultimos endpoints, y realizacion de pruebas

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
 namespace BackendServices.Controllers
 {
     [Route("api/[controller]")]
@@ -15,14 +15,20 @@ namespace BackendServices.Controllers
         private NpgsqlConnection connection;
         private string sql;
         private NpgsqlCommand cmd;
-        private DataTable dt;
 
-        
-
+        [HttpGet("people")]
+        public async Task<List<PeopleEntity>> GetPeople()
+        {
+            var listPeople = await GetListPeople();
+            if (listPeople.Count < 0)
+            {
+                return null;
+            }
+            return listPeople;
+        }
         [HttpGet("Tasks")]
         public async Task<List<TaskEntity>> GetTasks()
         {
-            
             var listTasks = await GetListTasks();
             if (listTasks.Count < 0)
             {
@@ -40,9 +46,52 @@ namespace BackendServices.Controllers
             }
             return task;
         }
-
+        [HttpPut("updateTask{idtask}*{idpeople}*{description}*{state}*{priority}*{fecInicio}*{fecFinal}*{notes}")]
+        public  Task<bool> updateTasks(int idtask,int idpeople,string description,string state, string priority,string fecInicio, string fecFinal,string notes)
+        {
+            Task<bool> num = updateTask(idtask,idpeople,description,state,priority,fecInicio,fecFinal, notes);
+            return num;
+        }
+        [HttpPost("insertTask{idpeople}*{description}*{state}*{priority}*{fecInicio}*{fecFinal}*{notes}")]
+        public Task<bool> insertTasks(int idpeople, string description, string state, string priority, string fecInicio, string fecFinal, string notes)
+        {
+            Task<bool> num = insertTask(idpeople, description, state, priority, fecInicio, fecFinal, notes);
+            return num;
+        }
+        [HttpDelete("deleteTask{id}")]
+        public async Task<bool> deleteTasks(int id)
+        {
+            bool task = await deleteTask(id);
+            return task;
+        }
 
         //DATABASE
+        private async Task<List<PeopleEntity>> GetListPeople()
+        {
+            List<PeopleEntity> listPeople = new List<PeopleEntity>();
+            connection = new NpgsqlConnection("Server= localhost; User Id= postgres; Password= 1234; Port= 5432; Database= backendservices");
+            try
+            {
+                connection.Open();
+                sql = @"SELECT * from public.sp_getPeople()";
+                cmd = new NpgsqlCommand(sql, connection);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listPeople.Add(new PeopleEntity()
+                    {
+                        idPeople = Int32.Parse(reader["v_idpeople"].ToString()),
+                        namePeople = reader["v_namepeople"].ToString()
+                    });
+                }
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+            }
+            return listPeople;
+        }
         private async Task<List<TaskEntity>> GetListTasks()
         {
             List<TaskEntity> listTasks = new List<TaskEntity>();
@@ -108,8 +157,71 @@ namespace BackendServices.Controllers
             }
             return task;
         }
+        private async Task<bool> updateTask(int idtask, int idpeople, string description, string state, string priority, string fecInicio, string fecFinal, string notes)
+        {
+            bool task = false;
+            connection = new NpgsqlConnection("Server= localhost; User Id= postgres; Password= 1234; Port= 5432; Database= backendservices");
+            try
+            {
+                connection.Open();
+                sql = @"SELECT public.sp_updateTask("+idtask+",'"+description+"','"+idpeople+"','"+state+"','"+priority+"','"+fecInicio+"','"+fecFinal+"','"+notes+"')";
+                cmd = new NpgsqlCommand(sql, connection);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                task = reader.Read();
+                while (reader.Read())
+                {
+                    task = true;
+                }
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+            }
+            return task;
+        }
+        private async Task<bool> insertTask(int idpeople, string description, string state, string priority, string fecInicio, string fecFinal, string notes)
+        {
+            bool task = false;
+            connection = new NpgsqlConnection("Server= localhost; User Id= postgres; Password= 1234; Port= 5432; Database= backendservices");
+            try
+            {
+                connection.Open();
+                sql = @"SELECT public.sp_insertTask('" + description + "','" + idpeople + "','" + state + "','" + priority + "','" + fecInicio + "','" + fecFinal + "','" + notes + "')";
+                cmd = new NpgsqlCommand(sql, connection);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                task = reader.Read();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+            }
+            return task;
+        }
+        private async Task<bool> deleteTask(int id)
+        {
+            bool task = false;
+            connection = new NpgsqlConnection("Server= localhost; User Id= postgres; Password= 1234; Port= 5432; Database= backendservices");
+            try
+            {
+                connection.Open();
+                sql = @"SELECT * from public.sp_deleteTask(" + id + ")";
+                cmd = new NpgsqlCommand(sql, connection);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                task = reader.Read();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+            }
+            return task;
+        }
+
 
     }
+    
 }
 
 
---
+++ Models/PeopleEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendServices.Models
+{
+    public class PeopleEntity
+    {
+        public int idPeople { get; set; }
+        public string namePeople { get; set; }
+    }
+}
